76b60f7f26fbc1473b2eac83b5e826d283d524bb
Juan Rodriguez <juanr.secades@gmail.com>
2021-05-22 14:58:03 +0200

    put good names to volumes (not working in m1)

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -1,5 +1,5 @@
 service mysql start
-cp -r -p /var/lib/mysql/ /Users/juanrodr/data/
+cp -r -p /var/lib/mysql/ /home/juanrodr/data/
 service mysql stop
 rm /etc/mysql/mariadb.conf.d/50-server.cnf
 mv -f ./50-server.cnf /etc/mysql/mariadb.conf.d/
