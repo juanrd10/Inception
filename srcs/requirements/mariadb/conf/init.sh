ae9dec929355dce6975c383e81b7d0941d3adeef
Juan Rodriguez <juanr.secades@gmail.com>
2021-05-22 12:40:12 +0200

    wp working and volumes advance

---
+++ srcs/requirements/mariadb/conf/init.sh
@@ -0,0 +1,7 @@
+service mysql start
+cp -r -p /var/lib/mysql/ /Users/juanrodr/data/
+service mysql stop
+rm /etc/mysql/mariadb.conf.d/50-server.cnf
+mv -f ./50-server.cnf /etc/mysql/mariadb.conf.d/
+service mysql start
+mysql -u root < ./db_server.sql
