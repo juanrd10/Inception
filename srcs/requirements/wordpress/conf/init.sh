19e62607d55ccb4077b69a662f77611671e76525
Juan Rodriguez <juanr.secades@gmail.com>
2021-06-22 00:36:13 +0200

    env variables of fastcgi and wordpress folder fixed

--- srcs/requirements/wordpress/conf/init.sh
+++ srcs/requirements/wordpress/conf/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR="/var/www/html/wordpress"
+DIR="/var/www/html/wordpress/wp-content"
 chown -R www-data:www-data /var/www/html/wordpress
 if [ ! -d "$DIR" ]; then
 mv /resources/wordpress/* /var/www/html/wordpress/
