f6c0dbcbedfcef5c618342d474aeb8df7ed9d8ee
Juan Rodriguez <juanr.secades@gmail.com>
2021-07-05 11:58:08 +0200

    working

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -1,3 +1,5 @@
+DIR="/var/lib/mysql"
+if [ ! -d "$DIR" ]; then
 chown -R mysql:mysql /var/lib/mysql
 mysql_install_db
 service mysql start
@@ -6,6 +8,7 @@ mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%'"
 mysql -e "FLUSH PRIVILEGES"
 service mysql stop
+fi
 rm /etc/mysql/mariadb.conf.d/50-server.cnf
 mv -f /dock_import/50-server.cnf /etc/mysql/mariadb.conf.d/
 exec /usr/bin/mysqld_safe
