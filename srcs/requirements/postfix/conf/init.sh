c538f1ff23dccc48a77fa005dc6ed08e191af897
Juan Rodriguez <juanr.secades@gmail.com>
2021-08-02 21:46:26 +0200

    postfix arguments fixed

--- srcs/requirements/postfix/conf/init.sh
+++
@@ -1,6 +0,0 @@
-echo "[smtp.gmail.com]:587    ${MAIL_USER}:${MAIL_PASS}" > /etc/postfix/sasl_passwd
-chmod 0400 /etc/postfix/sasl_passwd
-postmap /etc/postfix/sasl_passwd
-service rsyslog restart
-service postfix restart
-bash
